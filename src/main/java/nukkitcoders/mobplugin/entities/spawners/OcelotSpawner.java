f2ac2765586aae8b53027a2afdbcb321b9cd8c15
Vladimir <Vovo1179@gmail.com>
2018-08-22 15:34:18 +0300

    Update OcelotSpawner.java (#83)

--- src/main/java/nukkitcoders/mobplugin/entities/spawners/OcelotSpawner.java
+++ src/main/java/nukkitcoders/mobplugin/entities/spawners/OcelotSpawner.java
@@ -35,7 +35,9 @@ public class OcelotSpawner extends AbstractEntitySpawner {
         int blockId = level.getBlockIdAt((int) pos.x, (int) pos.y, (int) pos.z);
         int blockLightLevel = level.getBlockLightAt((int) pos.x, (int) pos.y, (int) pos.z);
 
-        if (blockId != Block.GRASS && blockId != Block.LEAVE) { // only spawns on gras or leave blocks
+        if (biomeId != 21 && biomeId != 149 && biomeId != 23 && biomeId != 151) {
+            result = SpawnResult.WRONG_BLOCK;
+        } else id(blockId != Block.GRASS && blockId != Block.LEAVE) { // only spawns on gras or leave blocks
             result = SpawnResult.WRONG_BLOCK;
 //        } else if (blockLightLevel < 9) { // uncommented because lightlevel doesn't work now
 //            result = SpawnResult.WRONG_LIGHTLEVEL;
