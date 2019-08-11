38b59a57b694a89165ca6f67a9fbf569b59e9251
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-08-11 17:48:42 +0300

    Apply #118 for mooshrooms too + version bump

--- src/main/java/nukkitcoders/mobplugin/entities/animal/walking/Mooshroom.java
+++ src/main/java/nukkitcoders/mobplugin/entities/animal/walking/Mooshroom.java
@@ -84,12 +84,12 @@ public class Mooshroom extends WalkingAnimal {
     public boolean onInteract(Player player, Item item) {
         super.onInteract(player, item);
         if (item.equals(Item.get(Item.BOWL, 0), true)) {
-            player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
             player.getInventory().addItem(Item.get(Item.MUSHROOM_STEW, 0, 1));
+            player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
             return true;
         } else if (item.equals(Item.get(Item.BUCKET, 0), true)) {
-            player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
             player.getInventory().addItem(Item.get(Item.BUCKET, 1, 1));
+            player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
             this.level.addSound(this, Sound.MOB_COW_MILK);
             return true;
         } else if (item.equals(Item.get(Item.WHEAT, 0)) && !this.isBaby()) {
