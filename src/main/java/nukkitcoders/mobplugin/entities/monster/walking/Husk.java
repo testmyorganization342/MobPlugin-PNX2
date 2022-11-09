974be5175432d5b85ba61cfa20467d2d0c145f52
Petteri <26197131+PetteriM1@users.noreply.github.com>
2022-11-09 21:09:40 +0200

    Update Husk.java

--- src/main/java/nukkitcoders/mobplugin/entities/monster/walking/Husk.java
+++ src/main/java/nukkitcoders/mobplugin/entities/monster/walking/Husk.java
@@ -1,10 +1,11 @@
 package nukkitcoders.mobplugin.entities.monster.walking;
 
+import cn.nukkit.Player;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityAgeable;
 import cn.nukkit.entity.EntitySmite;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
-import cn.nukkit.event.entity.EntityDamageEvent.DamageCause;
+import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
@@ -14,6 +15,7 @@ import nukkitcoders.mobplugin.route.WalkerRouteFinder;
 import nukkitcoders.mobplugin.utils.Utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Husk extends WalkingMonster implements EntityAgeable, EntitySmite {
@@ -56,9 +58,22 @@ public class Husk extends WalkingMonster implements EntityAgeable, EntitySmite {
     public void attackEntity(Entity player) {
         if (this.attackDelay > 23 && player.distanceSquared(this) <= 1) {
             this.attackDelay = 0;
-            player.attack(new EntityDamageByEntityEvent(this, player, DamageCause.ENTITY_ATTACK, getDamage()));
+            HashMap<EntityDamageEvent.DamageModifier, Float> damage = new HashMap<>();
+            damage.put(EntityDamageEvent.DamageModifier.BASE, this.getDamage());
+
+            if (player instanceof Player) {
+                float points = 0;
+                for (Item i : ((Player) player).getInventory().getArmorContents()) {
+                    points += this.getArmorPoints(i.getId());
+                }
+
+                damage.put(EntityDamageEvent.DamageModifier.ARMOR,
+                        (float) (damage.getOrDefault(EntityDamageEvent.DamageModifier.ARMOR, 0f) - Math.floor(damage.getOrDefault(EntityDamageEvent.DamageModifier.BASE, 1f) * points * 0.04)));
+            }
+            if (player.attack(new EntityDamageByEntityEvent(this, player, EntityDamageEvent.DamageCause.ENTITY_ATTACK, damage))) {
+                player.addEffect(Effect.getEffect(Effect.HUNGER).setDuration(140));
+            }
             this.playAttack();
-            player.addEffect(Effect.getEffect(Effect.HUNGER).setDuration(140));
         }
     }
 
